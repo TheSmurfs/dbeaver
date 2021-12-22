42f57f76c1c0c5aecb0ee5aa42d28594a95e3356
E1izabeth <kuzenkova.el@yandex.ru>
2021-12-22 19:07:14 +0300

    #14132 restore active result tab after refresh

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditor.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditor.java
@@ -3867,7 +3867,7 @@ public class SQLEditor extends SQLEditorBase implements
                     cr.viewer.updateFiltersText(false);
                 }
                 if (!result.hasError() && !queryProcessor.resultContainers.isEmpty()) {
-                    resultTabs.setSelection(queryProcessor.resultContainers.get(0).resultsTab);
+                    resultTabs.setSelection(activeResultsTab != null && !activeResultsTab.isDisposed()? activeResultsTab : queryProcessor.resultContainers.get(0).resultsTab);
                 }
                 // Set tab names by query results names
                 if (scriptMode || queryProcessor.getResultContainers().size() > 0) {
